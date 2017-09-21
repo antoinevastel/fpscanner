620e894a0ef650fb23b82c603edc33c8146de974
antoinevastel <antoine.vastel@gmail.com>
2017-09-21 14:30:42 +0200

    adds support for adblock, localstorage and cookies

--- src/fpscanner.js
+++ src/fpscanner.js
@@ -24,14 +24,14 @@ var fpscanner = (function () {
           /*"webGL",*/
           "userAgent",
           /*"dnt",*/
-          //"adBlock",
-          //"cookies",
+          "adBlock",
+          "cookies",
           "name",
           "version",
           "maths",
           //"productSub",
           //"navigatorPrototype",
-          /*"localStorage",*/
+          "localStorage",
         ]
       },
       {
@@ -130,6 +130,41 @@ var fpscanner = (function () {
           cosh(10),
           tanh(1)
         ].join(";");
+      },
+      adBlock: function() {
+        var ads = document.createElement("div");
+        ads.innerHTML = "&nbsp;";
+        ads.className = "adsbox";
+        var result = false;
+        try {
+          // body may not exist, that's why we need try/catch
+          document.body.appendChild(ads);
+          result = document.getElementsByClassName("adsbox")[0].offsetHeight === 0;
+          document.body.removeChild(ads);
+        } catch (e) {
+          result = false;
+        }
+        return result;
+      },
+      cookies: function() {
+        return window.navigator.cookieEnabled ? "yes" : "no";
+      },
+      localStorage: function() {
+        try {
+          localStorage.fp = "test";
+        } catch (ex) {}
+
+        try {
+          var domLocalStorage = "";
+          if (localStorage.fp == "test") {
+            domLocalStorage = "yes";
+          } else {
+            domLocalStorage = "no";
+          }
+        } catch (ex) {
+          domLocalStorage = "no";
+        }
+        return domLocalStorage;
       }
     },
     os : {
